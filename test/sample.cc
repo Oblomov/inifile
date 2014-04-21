92e0238648a395190a4a5b5e48637c2b5a361150
Giuseppe Bilotta <giuseppe.bilotta@gmail.com>
2014-04-21 11:19:44 +0200

    Bit more unit-testy sample

--- test/sample.cc
+++ test/sample.cc
@@ -13,49 +13,59 @@ int main(int argc, char *argv[])
 	IniFile ini;
 	int test = 0;
 
-#define TEST_STRING(text) \
+#define EXPECT_FAILURE(testcode, type) do { \
 	try { \
 		++test; \
-		ini = IniFile::parse(text); \
-		throw runtime_error("parsed correctly"); \
+		testcode; \
+		throw; \
+	} catch (type &e) { \
+		cerr << "[OK] (l." << __LINE__ << ") #" << test << ": " << #type << ": " << e.what() << endl; \
+	} catch (...) { \
+		cerr << "[FAIL] (l." << __LINE__ << ") #" << test << endl; \
+	} } while (0)
+
+#define EXPECT_SUCCESS(testcode) do { \
+	try { \
+		++test; \
+		testcode; \
+		cerr << "[OK] (l." << __LINE__ << ") #" << test << endl; \
 	} catch (runtime_error &e) { \
-		cerr << "error test #" << test << ": " << e.what() << endl; \
-	}
+		cerr << "[FAIL] (l." << __LINE__ << ") #" << test << ": " << e.what() << endl; \
+	} catch (...) { \
+		cerr << "[FAIL] (l." << __LINE__ << ") #" << test << endl; \
+	} } while (0)
 
-	TEST_STRING(" just a comment\n[section]\nkey=value");
-	TEST_STRING("# just a comment\n[section\nkey=value");
-	TEST_STRING("# just a comment\n[section \"]\nkey=value");
-	TEST_STRING("# just a comment\n[section \"open]\nkey=value");
-	TEST_STRING("# just a comment\n[section]\nkey value");
-	TEST_STRING("# just a comment\n[section]\nkey=value\n[override *]\nkey=valuestar\n[override spec]\nkeyspec=valuespec");
+#define TEST_PARSE_FAIL(text) \
+	EXPECT_FAILURE(ini = IniFile::parse(text), parse_error)
+#define TEST_PARSE_OK(text) \
+	EXPECT_SUCCESS(ini = IniFile::parse(text))
 
-#define TEST_VALUE(key, expected) \
-	try { \
-		++test; \
+	TEST_PARSE_FAIL(" just a comment\n[section]\nkey=value");
+	TEST_PARSE_FAIL("# just a comment\n[section\nkey=value");
+	TEST_PARSE_FAIL("# just a comment\n[section \"]\nkey=value");
+	TEST_PARSE_FAIL("# just a comment\n[section \"open]\nkey=value");
+	TEST_PARSE_FAIL("# just a comment\n[section]\nkey value");
+	TEST_PARSE_OK("# just a comment\n[section]\nkey=value\n[override *]\nkey=valuestar\n[override spec]\nkeyspec=valuespec");
+
+#define TEST_VALUE_FAIL(key) \
+	EXPECT_FAILURE(ini.get(key), notfound_error)
+#define TEST_VALUE_IS(key, expected) \
+	EXPECT_SUCCESS( \
 		string got = ini.get(key); \
 		if (got != expected) { \
 			stringstream error; \
 			error << "expected " << expected ", got " << got; \
 			throw runtime_error(error.str()); \
 		} \
-	} catch (notfound_error &e) { \
-		cerr << "error test #" << test << ": not found: " << e.what() << endl; \
-	} catch (runtime_error &e) { \
-		cerr << "error test #" << test << ": " << e.what() << endl; \
-	}
+		)
 
-	TEST_VALUE("section", "fail");
-	TEST_VALUE("section.key", "should be value");
-	TEST_VALUE("override.spec.key", "should be valuestar");
-	TEST_VALUE("override.spec.keyspec", "should be valuespec");
+	TEST_VALUE_FAIL("section");
+	TEST_VALUE_IS("section.key", "value");
+	TEST_VALUE_IS("override.spec.key", "valuestar");
+	TEST_VALUE_IS("override.spec.keyspec", "valuespec");
 
 
-	try {
-		++test;
-		ini = IniFile::load("this file doesn't exist");
-	} catch (notfound_error &e) {
-		cerr << "error test #" << test << ": not found (file): " << e.what() << endl;
-	}
+	EXPECT_FAILURE(ini = IniFile::load("this file doesn't exist"), notfound_error);
 
 	if (argc > 1) {
 		ini = IniFile::load(argv[1]);
